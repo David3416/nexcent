1f215d6e7d00c838406ebb0be5d38f15e5efc5b9
David3415 <dgagiev@gmail.com>
2026-07-28 18:19:10 +0300

    Styles and fonts added

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../views')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
- res.sendFile(path.join(__dirname, '../views', 'index.html'));
+ res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 app.listen(PORT, () => {
